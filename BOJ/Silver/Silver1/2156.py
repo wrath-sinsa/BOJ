a9edce3db964203a8c050cea4dfd13a0594c7c70
wrath-sinsa <73694022+wrath-sinsa@users.noreply.github.com>
2021-04-20 11:27:24 +0900

    2156

--- BOJ/Silver/Silver1/2156.py
+++ BOJ/Silver/Silver1/2156.py
@@ -10,14 +10,17 @@ pop = heapq.heappop
 
 N = int(input())
 #N, M = map(int,input().split())
-lst = [0] * (N+1)
+lst = [0] * (10000+1)
 for i in range (1, N+1) :
     lst[i] = int(input())
 
-DP = [0] * (N+1)
+DP = [0] * (10000+1)
 DP[1] = lst[1]
 DP[2] = lst[1] + lst[2]
-DP[3] = max([DP[2], lst[1] + lst[3], lst[2] + lst[3]])
-DP[4] = max([Dp[2]+lst[4], lst[1]+lst[3]+lst[4], lst[2]+lst[3]])
+DP[3] = max([DP[2], DP[1] + lst[3], DP[0] + lst[2] + lst[3]])
+DP[4] = max([DP[2]+lst[4], DP[1]+lst[3]+lst[4], lst[2]+lst[3]])
 for i in range (5, N+1) :
-    DP[i] = max()
+    DP[i] = max(DP[i-2], DP[i-3]+lst[i-1]) + lst[i]
+    DP[i] = max(DP[i], DP[i-1])
+
+print(DP[N])
